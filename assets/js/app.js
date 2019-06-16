0bea13b90808c7d36f793e535ffa1f57346ae3e7
GeraldasK <gerk.kasp@icloud.com>
2019-06-16 22:09:17 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+$(document).ready(function () {
+    alert("Hello! I am an alert box!!");
+});
+
+// console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+function validateLoginForm() {
+    let email =
+}
